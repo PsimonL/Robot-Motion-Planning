6aac891b481ac1fa175e913e948f94224758bb79
Szymon Rogowski <szymon0r0official@gmail.com>
2023-07-19 22:40:20 +0200

    env in progress

--- reinforcment-learning-DQN/environment.py
+++ reinforcment-learning-DQN/environment.py
@@ -1,6 +1,8 @@
 import pygame
 import math
 import shapes
+import geometry
+
 
 class RobotSimulation:
     def __init__(self):
@@ -20,7 +22,11 @@ class RobotSimulation:
         pygame.display.set_caption("Automatic robot simulation")
         self.clock = pygame.time.Clock()
 
-        self.robot = shapes.FloatRect(10, 10, 25, 25)
+        self.start_x = 10
+        self.start_y = 10
+        self.finish_x = 200
+        self.finish_y = 200
+        self.robot = shapes.FloatRect(self.start_x, self.start_y, 25, 25)
 
         self.trail_points = []
 
@@ -32,21 +38,21 @@ class RobotSimulation:
     def move_robot(self, action):
         dx, dy = 0, 0
 
-        if action == 0:  # move left
+        if action == 1:  # move left
             dx = -1
-        elif action == 1:  # move right
+        elif action == 2:  # move right
             dx = 1
-        elif action == 2:  # move up
+        elif action == 3:  # move up
             dy = -1
-        elif action == 3:  # move down
+        elif action == 4:  # move down
             dy = 1
-        elif action == 4:  # move up and left
+        elif action == 5:  # move up and left
             dx, dy = -1, -1
-        elif action == 5:  # move up and right
+        elif action == 6:  # move up and right
             dx, dy = 1, -1
-        elif action == 6:  # move down and left
+        elif action == 7:  # move down and left
             dx, dy = -1, 1
-        elif action == 7:  # move down and right
+        elif action == 8:  # move down and right
             dx, dy = 1, 1
 
         speed = 1
@@ -56,20 +62,46 @@ class RobotSimulation:
         self.trail_points.append((self.robot.x + self.robot.width / 2, self.robot.y + self.robot.height / 2))
 
     def reset(self):
-        pass
+        self.robot.x = self.start_x
+        self.robot.y = self.start_y
+        self.trail_points = []
+
+    def calculate_distance_to_finish(self):
+        distance_x = self.finish_x - self.robot.x
+        distance_y = self.finish_y - self.robot.y
+        return math.sqrt(distance_x ** 2 + distance_y ** 2)
 
     def step(self, action):
         next_state = (self.robot.x, self.robot.y)
         self.move_robot(action)
 
-        reward = -1
+        distance_to_finish = self.calculate_distance_to_finish()
+
+        # reward for becoming closer to aim
+        reward = -0.01 * distance_to_finish
+
         done = False
 
-        if self.robot.x > 500 and self.robot.y > 500:
+        # end if finish
+        if distance_to_finish < 20:
             reward = 100
             done = True
+            self.reset()
 
-        return next_state, reward, done, {}
+        # collision penalty
+        for obstacle in self.obstacles:
+            if geometry.check_collision(self.robot, obstacle.rect):
+                reward = -50
+                done = True
+                self.reset()
+
+        # too long time penalty
+        if len(self.trail_points) > 1000:
+            reward = -100
+            done = True
+            self.reset()
+
+        return next_state, reward, done
 
     def main(self):
         runner = True
@@ -79,13 +111,12 @@ class RobotSimulation:
                 if event.type == pygame.QUIT:
                     runner = False
 
+            action = 8
 
-            action = 1
-
-
-
-            next_state, reward, done, _ = self.step(action)
+            next_state, reward, done = self.step(action)
             current_state = next_state
+            print(f"current_state = {current_state}")
+            print(f"reward = {reward}")
 
             self.screen.fill(self.BLACK)
 
@@ -100,6 +131,7 @@ class RobotSimulation:
 
             pygame.display.update()
         pygame.quit()
+
 
 if __name__ == '__main__':
     simulation = RobotSimulation()
